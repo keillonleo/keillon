1532e2bc9ead897c58afb2169d0b00d8cdb38030
Keillon <keillonleo1010@gmail.com>
2018-12-15 04:44:12 -0300

    Keillon

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+    define('SITE_HOME', 'http://localhost/projeto-dsi-2018');
+    define('SITE', 'Sistema de Gerenciamento de Trabalhos Acadêmicos');
+
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'seminarios');
+    define('DB_USER', 'root');
+    define('DB_PWD', '');
+?>
